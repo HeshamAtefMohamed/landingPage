04136458151623471d2c722879b0cee2a3d9133f
Hesahm Atef Mohamed <90654024+HeshamAtefMohamed@users.noreply.github.com>
2021-11-14 13:28:29 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -60,35 +60,33 @@ for(section of sections){
 // determine which section is in viewport
 
 //Build the viewport function to check which section is in viewport and active
-    const isInViewport= function (section) {
-            //use section.getBoundingClientRect() method to determine section position from all dimensions in the viewoirt
-            const bounding = section.getBoundingClientRect();
-            return (
-                bounding.top >= 0 &&
-                bounding.left >= 0 &&
-                bounding.bottom <=110 &&
-                bounding.right <= window.innerWidth
-            );
-        };  
-    
-
+const isInViewport= (section) => {
+    //use section.getBoundingClientRect() method to determine section position from all dimensions in the viewoirt
+    const bounding = section.getBoundingClientRect();
+    return (
+    // it will check the function and work clearly wether in pc or small devices
+       bounding.top >= 0 &&
+       bounding.left >= 0 &&
+       bounding.top <=250 &&
+       bounding.right <= window.innerWidth    );
+}; 
 // build a function to give the active class style to section in viewport
 
 const activeClass = ()=>{
-    //looping over sections and give active class to section in viewport
-    for(section of sections){
-        //condition to detect which section is in the viewport
-        if(isInViewport){ 
-        //add active class if condition is true 
-        section.classList.add("your-active-class");
-        }
-        else{
-        // remove active class if condition is false
-        section.classList.remove("your-active-class");    
-        }
-    };
+//looping over sections and give active class to section in viewport
+for(section of sections){
+//condition to detect which section is in the viewport and pass the section as an argument to the isInViewport function
+if(isInViewport(section)){ 
+//add active class if condition is true 
+section.classList.add("your-active-class");
+}
+else{
+// remove active class if condition is false
+section.classList.remove("your-active-class");    
+}
+};
 };
 // build the event that when scroll in the window the section in the viewport take the active class
-    window.addEventListener("scroll", activeClass);
+window.addEventListener("scroll", activeClass);
 
 
